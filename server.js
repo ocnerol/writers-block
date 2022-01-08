168ec223fa33506d54dff7084b260c9f9ec9fd5d
Lorenço <lorenco.alvareznavarrete@gmail.com>
2022-01-08 21:26:28 +0000

    Rename widgets routes info from template to routes

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const storiesRoutes = require("./routes/stories");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+app.use("/stories", storiesRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 // Home page
