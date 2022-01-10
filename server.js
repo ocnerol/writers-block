e5e50d44a8dd80b9dfd96bfbf41e67ead7538819
houseofsam <samxsaid@gmail.com>
2022-01-09 19:44:08 -0500

    Implement user sessions and users/login/:id route

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieSession = require('cookie-session');
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -18,6 +19,11 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1']
+}));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -41,8 +47,8 @@ const storiesRoutes = require("./routes/stories");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/contributions", contributionsRoutes(db));
+app.use("/users", usersRoutes(db));
+app.use("/contributions", contributionsRoutes(db));
 app.use("/stories", storiesRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
