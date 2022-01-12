a14b6aac32f58770a966a75fa688b33efd7b443b
Karen <to.karenngo@gmail.com>
2022-01-12 18:40:26 -0500

    Add mark complete/ongoing feature that updates database and hides and reveals needed elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -32,7 +32,7 @@ $(() => { //once document is loaded/ready...
   const displayFullContributions = function () {
     $('.full-contribution-container').addClass('hidden');
     $('.contribution-container').removeClass('hidden');
-    $('.add-block-btn').removeClass('hidden');
+    //$('.add-block-btn').removeClass('hidden');
     $('.new-block').addClass('hidden');
     $('.back-to-blocks').addClass('hidden');
   }
@@ -87,12 +87,70 @@ $(() => { //once document is loaded/ready...
         $(".full-contribution-container").addClass('hidden')
         console.log('userID------>', userID)
         if (response.story_author_id === userID) {
+          if (response.is_complete) {
+            $(".complete-toggle").addClass('pressed-complete')
+            $(".ongoing-toggle").removeClass('pressed-ongoing')
+            $('.add-block-btn').addClass('hidden');
+          } else {
+            $(".complete-toggle").removeClass('pressed-complete')
+            $(".ongoing-toggle").addClass('pressed-ongoing')
+          }
           console.log('response.story_author_id', response.story_author_id)
           $("#complete").addClass('hidden')
           $(".complete-toggle").removeClass('hidden')
           $(".ongoing-toggle").removeClass('hidden')
           $("#author").addClass('hidden')
           $("#genre").addClass('hidden')
+
+      // --------- If author, when clicking the COMPLETE button, add class
+          $(".complete-toggle").click(function() {
+            const storyID = $('body').attr('data-story-id')
+            //console.log('storyID------>', storyID)
+            $.post(`http://localhost:8080/stories/${storyID}/complete`)
+            $(".complete-toggle").addClass('pressed-complete')
+            $(".ongoing-toggle").removeClass('pressed-ongoing')
+
+            //Hide 'Add a block'
+            $(".add-block-btn").addClass('hidden')
+
+            //Hide --->
+              //$(".").addClass('hidden')
+
+            //Hide ---> 'Back to blocks'
+            $(".back-to-blocks").addClass('hidden')
+
+            //Hide ---> contribution container preview tiles
+            $(".contribution-container").removeClass('hidden')
+            $(".full-contribution-container").addClass('hidden');
+
+
+          });
+
+      //---------- If author, when clicking the ONGOING button, add class
+          $(".ongoing-toggle").click(function() {
+            const storyID = $('body').attr('data-story-id')
+            $.post(`http://localhost:8080/stories/${storyID}/ongoing`)
+            $(".complete-toggle").removeClass('pressed-complete')
+            $(".ongoing-toggle").addClass('pressed-ongoing')
+
+            //Show ---> add block
+            $(".add-block-btn").removeClass('hidden')
+
+            //Hide ---> full preview of the spec
+           // $(".contribution-container").removeClass('hidden')
+
+            //Hide ---> 'Back to blocks'
+            //$(".back-to-blocks").removeClass('hidden')
+
+            //Show ---> contribution container IF full-contribution is hidden
+            $(".contribution-container").removeClass('hidden')
+
+
+
+          });
+
+
+
           //default complete + inprogress buttons as hidden --> use  .hide/.show THEN SHOW
           //default merge button hidden, show
         }
