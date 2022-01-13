f8ec97f3fc06d5fbf1970a247bd98738f60aad8c
houseofsam <samxsaid@gmail.com>
2022-01-13 01:35:01 -0500

    Make upvote and downvote buttons functional
    Change colours of vote arrows upon click, increment and change count value displayed next to arrows, prevent non-logged in users from voting, prevent consecutive voting

--- public/scripts/upvotes.js
+++ public/scripts/upvotes.js
@@ -1,32 +1,56 @@
 $(document).ready(() => {
-
   let isUpvoteClicked = false;
   let isDownvoteClicked = false;
+  console.log(storyContributions);
 
-  $('.wrapper-story').on('click', '.fa-chevron-up', function(e) {
-    const contributionID = $(this).closest('.full-contribution-container').attr('data-id');
+  $(".wrapper-story").on("click", ".fa-chevron-up", function () {
+    // do nothing if user not logged in
+    if (!userID) {
+      return;
+    }
+
+    const contributionID = $(this)
+      .closest(".full-contribution-container")
+      .attr("data-id");
+
     if (isUpvoteClicked) {
       return;
     }
     // change clicked on arrow to red
     $(this).css("color", "red");
     // change the opposite arrow to purple
-    $(this).siblings('.fa-chevron-down').css("color", "#6117ff");
-    $.post(`http://localhost:8080/contributions/${contributionID}/upvote`)
-    .then(() => {
-      isUpvoteClicked = true;
-      isDownvoteClicked = false;
-      // console.log($('.wrapper-story').find('tag'));
-      console.log('done post upvote');
-      console.log('iuc', isUpvoteClicked);
-      console.log('idc', isDownvoteClicked);
-    })
+    $(this).siblings(".fa-chevron-down").css("color", "#6117ff");
+    $.post(`http://localhost:8080/contributions/${contributionID}/upvote`).then(
+      () => {
+        // Get current count value and increment
+        let currentVoteCount = Number(
+          $(this).siblings(`.tag${contributionID}`).text()
+        );
+        // If the other vote button was previously clicked, increment by 2
+        let newVoteCount = isDownvoteClicked
+          ? currentVoteCount + 2
+          : currentVoteCount + 1;
+        let newCountDisplay = newVoteCount.toString();
+        // console.log('cvc', currentVoteCount, 'nvc', newVoteCount, 'ncd', newCountDisplay);
+        $(`.tag${contributionID}`).text(newCountDisplay);
 
-
+        // change "state" and prevent consecutive clicks
+        isUpvoteClicked = true;
+        isDownvoteClicked = false;
+      }
+    );
   });
 
-  $('.wrapper-story').on('click', '.fa-chevron-down', function(e){
-    const contributionID = $(this).closest('.full-contribution-container').attr('data-id');
+  $(".wrapper-story").on("click", ".fa-chevron-down", function () {
+    // do nothing if user not logged in
+    if (!userID) {
+      return;
+    }
+
+    const contributionID = $(this)
+      .closest(".full-contribution-container")
+      .attr("data-id");
+
     // do nothing if it has already been clicked on
     if (isDownvoteClicked) {
       return;
@@ -34,16 +58,23 @@ $(document).ready(() => {
     // change clicked on arrow to red
     $(this).css("color", "red");
     // change the opposite arrow to purple
-    $(this).siblings('.fa-chevron-up').css("color", "#6117ff");
-    $.post(`http://localhost:8080/contributions/${contributionID}/downvote`)
-    .then(() => {
+    $(this).siblings(".fa-chevron-up").css("color", "#6117ff");
+    $.post(
+      `http://localhost:8080/contributions/${contributionID}/downvote`
+    ).then(() => {
+      // Get current count value and increment
+      let currentVoteCount = Number(
+        $(this).siblings(`.tag${contributionID}`).text()
+      );
+      // If the other vote button was previously clicked, increment by 2
+      let newVoteCount = isUpvoteClicked
+        ? currentVoteCount - 2
+        : currentVoteCount - 1;
+      let newCountDisplay = newVoteCount.toString();
+      $(`.tag${contributionID}`).text(newCountDisplay);
+
       isDownvoteClicked = true;
       isUpvoteClicked = false;
-      console.log('done post downvote');
-      console.log('iuc', isUpvoteClicked);
-      console.log('idc', isDownvoteClicked);
-
-      })
     });
-
+  });
 });
