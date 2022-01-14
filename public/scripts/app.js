519da8597ffe08da0ab05d6307f477b202dd64ab
Karen <to.karenngo@gmail.com>
2022-01-14 02:03:53 -0500

    Add landing div to slide down for new users

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -36,6 +36,7 @@
 
 
 $(() => { //once document is loaded/ready...
+
   mergeContribution = function(storyID, contributionID) {
     $.post(`/contributions/mark-as-merged/${contributionID}`)
       .catch(error => console.log(error, error.message));
@@ -106,8 +107,6 @@ const loadStory = function() {
   $.get(`/stories/${storyID}/data`) //using AJAX to fetch data
     .then((response) => {
       //console.log('response------>', response)
-      //$("#all-tweets").empty();
-      //renderTweets(response);
       const pendingContributions = response.contributions.filter(contribution => !contribution.contribution_is_accepted);
       const acceptedContributions = response.contributions.filter(contribution => contribution.contribution_is_accepted);
       $("#author").text(`- ${response.author_name}`)
@@ -135,6 +134,7 @@ const loadStory = function() {
       // !!! ^^^
       $(".full-contribution-container").addClass('hidden')
       console.log('userID------>', userID)
+
       if (response.story_author_id === userID) {
         if (response.is_complete) {
           $(".complete-toggle").addClass('pressed-complete')
@@ -163,6 +163,7 @@ const loadStory = function() {
           .catch((error) => {
             console.log('Error while loading story', error);
           });
+//-------------------------SHOW CREATE A STORY BUTTON ON SPECIFIC STORY PAGE IF LOGGED IN
 
         // --------- If author, when clicking the COMPLETE button, add class
         $(".complete-toggle").click(function() {
@@ -176,9 +177,6 @@ const loadStory = function() {
           $(".add-block-btn").addClass('hidden')
           //Hide 'BLOCK FORM'
           $(".new-block").addClass('hidden')
-
-
-
 
           //Hide ---> 'Back to blocks'
           $(".back-to-blocks").addClass('hidden')
--- public/scripts/home.js
+++ public/scripts/home.js
@@ -2,15 +2,7 @@ $(() => {
 
 
 
-  $(window).bind('beforeunload',function(){
 
-    const userID = $("h1").attr("data-id");
-    $(".landing-div").slideUp(0);
-    if (!userID) {
-      $(".landing-div").slideDown(0);
-    }
-
-});
 
   const userID = $("h1").attr("data-id");
   $(".landing-div").slideUp(0);
@@ -24,15 +16,64 @@ $(() => {
       $(".landing-div").hide();
     },2000 )
 
-  })
-//TEST
-  $('.login-button').on('click', (e) => {
-    e.preventDefault();
-    $.post(`http://localhost:8080/users/login/2`)
-    .then(() => {
-      // location.reload();
-    })
-  })
-
+  });
 
 });
+
+// $(() => {
+//   // if (!userID) {
+//   //   $(".create-story-btn").hide();
+//   //   $('.add-block-btn').addClass('hidden');
+//   // } else {
+//   //   $(".create-story-btn").show();
+//   //   $('.add-block-btn').removeClass('hidden');
+//   // }
+
+
+
+// //   $(window).bind('beforeunload',function(){
+// //     const userID = $("h1").attr("data-id");
+// //     $(".landing-div").slideUp(0);
+// //     if (!userID) {
+// //       $(".landing-div").slideDown(0);
+// //     }
+// // });
+
+//   const userID = $("h1").attr("data-id");
+//   $(".landing-div").slideUp(0);
+//   if (!userID) {
+//     $(".landing-div").slideDown(0);
+//     // $(".create-story-btn").hide();
+//     // $('.add-block-btn').addClass('hidden');
+//   } else {
+//     $(".landing-div").slideUp(0);
+//   //   $(".create-story-btn").show();
+//   //   $('.add-block-btn').removeClass('hidden');
+//   // }
+
+//   $(".get-started-btn").click(function() {
+//     $(".landing-div").slideUp(2000);
+//     setTimeout(()=>{
+//       $(".landing-div").hide();
+//     },2000 )
+//   })
+
+//   // $(".login-button").click(function() {
+//   //   $(".create-story-btn").show();
+//   //   $('.add-block-btn').removeClass('hidden');
+//   // })
+
+
+
+
+
+
+
+// //TEST
+//   // $('.login-button').on('click', (e) => {
+//   //   e.preventDefault();
+//   //   $.post(`http://localhost:8080/users/login/2`)
+//   //   .then(() => {
+//   //     // location.reload();
+//   //   })
+//   // })
