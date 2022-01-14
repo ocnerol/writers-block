b746b8ba296a6ee10f7dae6a2d02bb398fc4fedb
Karen <to.karenngo@gmail.com>
2022-01-13 22:24:48 -0500

    Add slide up and down script for landing div

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,11 +1,29 @@
 $(() => {
 
+
+
+  $(window).bind('beforeunload',function(){
+
+    const userID = $("h1").attr("data-id");
+    $(".landing-div").slideUp(0);
+    if (!userID) {
+      $(".landing-div").slideDown(0);
+    }
+
+});
+
   const userID = $("h1").attr("data-id");
   $(".landing-div").slideUp(0);
   if (!userID) {
     $(".landing-div").slideDown(0);
   }
 
+  $(".get-started-btn").click(function() {
+    $(".landing-div").slideUp(2000);
+    setTimeout(()=>{
+      $(".landing-div").hide();
+    },2000 )
 
+  });
 
 });
